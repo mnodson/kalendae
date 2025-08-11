59ddc30db538ca643a71b9757abcdc307814b33a
Mark Nodson <mark.nodson@fleetpride.com>
2025-08-11 11:30:08 -0500

    feat: firebase setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { Firestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true
 })
-export class AppComponent {
-  title = 'kalendae';
+export class AppComponent implements OnInit {
+    constructor(private firestore: Firestore) {}
+
+
+    ngOnInit() {
+        // Initialize Firestore or perform any setup needed
+        // this.firestore.collection('exampleCollection').valueChanges().subscribe(data => {
+        //     console.log('Data from Firestore:', data);
+        // });
+    }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,14 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideFirebaseApp(() => initializeApp({ projectId: "kalendae-da135", appId: "1:860250158150:web:714f198575dda6ed550647", storageBucket: "kalendae-da135.firebasestorage.app", apiKey: "AIzaSyB4zLr3eG4jgWyv1TFO_fufMwqTfnwgEhI", authDomain: "kalendae-da135.firebaseapp.com", messagingSenderId: "860250158150", measurementId: "G-PLNTKJCDW8" })), provideStorage(() => getStorage()),
+    provideFirestore(() => getFirestore())]
 };
